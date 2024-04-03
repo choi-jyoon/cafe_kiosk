0e8b67c75112c222fef81964efff44b0ffd656a2
choi-jyoon <72444141+choi-jyoon@users.noreply.github.com>
2024-04-03 17:25:23 +0900

    feat: model 생성

--- cafe/models.py
+++ cafe/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=200)
+
+
+class Menu(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    menu_name = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
+    stock = models.IntegerField(default=0)
